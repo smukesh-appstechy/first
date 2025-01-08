133f4443922786c3da67c5e37dd3b219d326cd7d
mukesh <smukesh@appstechy.com>
2025-01-08 12:30:29 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './layout.jsx'
+import Layout from "./Layout.jsx"
 import Home from './components/home/Home.jsx'
 import About from './components/about/About.jsx'
 import Blog from './components/blog/Blog.jsx'
